a259dcbc299f604c77d0c03a385cadf3f027bd7d
yuna0102 <mkiyg@naver.com>
2020-08-04 02:45:41 +0900

    crud_create

--- page/views.py
+++ page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Designer
 
 # Create your views here.
@@ -12,3 +12,28 @@ def introduce(request):
 def detail(request, designer_id):
     designer = get_object_or_404(Designer, pk = designer_id)
     return render(request, 'detail.html', {'designer' : designer})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    if request.method == 'POST':
+        post = Designer()
+
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']
+        post.name = request.POST['name']
+        post.address = request.POST['address']
+        post.description = reqeust.POST['description']
+
+        post.save()
+
+        return redirect('detail', post.id)
+        #redirect는 주소로 이동만 함
+        #render는 어떤 값을 요청하고 받아들임
+
+def delete(request, designer_id):
+    post = get_object_or_404(Designer, pk = designer_id)
+    post.delete()
+
+    return redirect('home')
